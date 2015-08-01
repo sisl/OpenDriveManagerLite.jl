800075ae8452d24f3b0bd809357e450b455197ae
depet6 <depet6@dn51s4rj.sunet>
2015-07-31 20:49:04 -0700

    misc

--- test/test_odrmanagerlite.jl
+++ test/test_odrmanagerlite.jl
@@ -1,3 +1,3 @@
-# mgr = OdrManagerLite()
-# loadfile(mgr, "/home/tim/V1.4.3lite/data/sample1.1.xodr")
-# printdata(mgr)
+ mgr = OdrManagerLite()
+ loadfile(mgr, "/home/tim/V1.4.3lite/data/sample1.1.xodr")
+ printdata(mgr)
