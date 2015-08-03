579759d4f6881f32b077f2a4df7374e01aece50b
depet6 <depet6@dn51s4s5.sunet>
2015-08-02 18:13:25 -0700

    misc

--- src/includes/odrcoord.jl
+++ src/includes/odrcoord.jl
@@ -1,17 +1,38 @@
 export
 
-print_coord
+coord_equal,
+coord_multiply,
+coord_plus,
+coord_subtract,
+coord_plusequal,
+coord_minusequal,
+get_X,
+get_Y,
+get_Z,
+get_H,
+get_P,
+get_R,
+set_coord,
+setX_coord,
+setY_coord,
+setZ_coord,
+setH_coord,
+setP_coord,
+setR_coord,
+intitialize_coord,
+print_coord,
+get_value_coord
 
 type Coord
-    x::Cdouble;
-    y::Cdouble;
-    z::Cdouble;
-    h::Cdouble;
-    p::Cdouble;
-    r::Cdouble;
+    x::Cdouble
+    y::Cdouble
+    z::Cdouble
+    h::Cdouble
+    p::Cdouble
+    r::Cdouble
 
-    Deg2Rad::Cdouble; 
-    Rad2Deg::Cdouble;
+    Deg2Rad::Cdouble 
+    Rad2Deg::Cdouble
 
     Coord()=new()
     Coord(x::Real, y::Real, z::Real, h::Real=0.0, p::Real=0.0, r::Real=0.0) =
@@ -20,7 +41,7 @@ type Coord
     function Coord() 
         coord = new() # create one without setting any values
         finalizer(coord, obj ->begin
-            ccall((:freeTrackCoord, LIB_ODRMGR), Void, (Ptr{Void},), obj.prt)
+            ccall((:freeCoord, LIB_ODRMGR), Void, (Ptr{Void},), obj.prt)
         end)
         ptr
     end
@@ -34,63 +55,88 @@ type Coord
 
 end
 
-
-function coord_plusequal(a::TrackCoord, b::TrackCoord)
+function coord_equal(a::CoordA, b::CoordB)
     (ccall( (:coord_plusequal, LIB_ODRMGR), Void, (Ptr{Void},Ptr{Void}), 
         pointer_from_objref(a), pointer_from_objref(b)))
     a
 end
+function coord_multiply(a::CoordA, b::CoordB)
+    (ccall( (:coord_plusequal, LIB_ODRMGR), Void, (Ptr{Void},Ptr{Void}), 
+        pointer_from_objref(a), pointer_from_objref(b)))
+    a
+end
+function coord_plus(a::CoordA, b::CoordB)
+    (ccall( (:coord_plusequal, LIB_ODRMGR), Void, (Ptr{Void},Ptr{Void}), 
+        pointer_from_objref(a), pointer_from_objref(b)))
+    a
+end
+function coord_subtract(a::CoordA, b::CoordB)
+    (ccall( (:coord_plusequal, LIB_ODRMGR), Void, (Ptr{Void},Ptr{Void}), 
+        pointer_from_objref(a), pointer_from_objref(b)))
+    a
+end
+function coord_plusequal(a::CoordA, b::CoordB)
+    (ccall( (:coord_plusequal, LIB_ODRMGR), Void, (Ptr{Void},Ptr{Void}), 
+        pointer_from_objref(a), pointer_from_objref(b)))
+    a
+end
+function coord_minusequal(a::CoordA, b::CoordB)
+    (ccall( (:coord_plusequal, LIB_ODRMGR), Void, (Ptr{Void},Ptr{Void}), 
+        pointer_from_objref(a), pointer_from_objref(b)))
+    a
+end
+function get_X(coord::Coord) 
+    ptr = ccall((:coord_getX, LIB_ODRMGR), Double, (Ptr{Void},), coord.ptr)
+    return unsafe_load(ptr, 1)::Double
+end
+function get_Y(coord::Coord) 
+     ptr = ccall((:coord_getY, LIB_ODRMGR), Double, (Ptr{Void},), coord.ptr)
+    return unsafe_load(ptr, 1)::Double
+end
+function get_Z(coord::Coord) 
+     ptr = ccall((:coord_getZ, LIB_ODRMGR), Double, (Ptr{Void},), coord.ptr)
+    return unsafe_load(ptr, 1)::Double
+end
+function get_H(coord::Coord) 
+     ptr = ccall((:coord_getH, LIB_ODRMGR), Double, (Ptr{Void},), coord.ptr)
+    return unsafe_load(ptr, 1)::Double
+end
+function get_P(coord::Coord) 
+     ptr = ccall((:coord_getP, LIB_ODRMGR), Double, (Ptr{Void},), coord.ptr)
+    return unsafe_load(ptr, 1)::Double
+end
+function get_R(coord::Coord) 
+    ptr = ccall((:coord_getR, LIB_ODRMGR), Double, (Ptr{Void},), coord.ptr)
+    return unsafe_load(ptr, 1)::Double
+end
 
-function get_offset(coord::TrackCoord) 
-    ccall((:coord_getX, LIB_ODRMGR), Double, (Ptr{Void},), coord.ptr)
-    return coord->getOffset();
-end
-function get_off(coord::TrackCoord) 
-    ccall((:coord_getY, LIB_ODRMGR), Double, (Ptr{Void},), coord.ptr)
-    return coord->getOffset();
-end
-function get_offset(coord::TrackCoord) 
-    ccall((:coord_getZ, LIB_ODRMGR), Double, (Ptr{Void},), coord.ptr)
-    return coord->getOffset();
-end
-function get_offset(coord::TrackCoord) 
-    ccall((:coord_getH, LIB_ODRMGR), Double, (Ptr{Void},), coord.ptr)
-    return coord->getOffset();
-end
-function get_offset(coord::TrackCoord) 
-    ccall((:coord_getP, LIB_ODRMGR), Double, (Ptr{Void},), coord.ptr)
-    return coord->getOffset();
-end
-function get_offset(coord::TrackCoord) 
-    ccall((:coord_getR, LIB_ODRMGR), Double, (Ptr{Void},), coord.ptr)
-    return coord->getOffset();
-end
-
-
-
-set_coord(coord::TrackCoord, value::Int) =
+set_coord(coord::Coord, value::Int) =
     ccall((:coord_set, LIB_ODRMGR), Void, (Ptr{Void}, Ptr{Integer}), coord.ptr, value)
 
-setX_coord(coord::TrackCoord, value::Int) =
+setX_coord(coord::Coord, value::Int) =
     ccall((:coord_setX, LIB_ODRMGR), Void, (Ptr{Void}, Ptr{Integer}), coord.ptr, value)
 
-setY_coord(coord::TrackCoord, value::Int) =
+setY_coord(coord::Coord, value::Int) =
     ccall((:coord_setY, LIB_ODRMGR), Void, (Ptr{Void}, Ptr{Integer}), coord.ptr, value)
 
-setZ_coord(coord::TrackCoord, value::Int) =
+setZ_coord(coord::Coord, value::Int) =
     ccall((:coord_setZ, LIB_ODRMGR), Void, (Ptr{Void}, Ptr{Integer}), coord.ptr, value)
 
-setH_coord(coord::TrackCoord, value::Int) =
+setH_coord(coord::Coord, value::Int) =
     ccall((:coord_setH, LIB_ODRMGR), Void, (Ptr{Void}, Ptr{Integer}), coord.ptr, value)
 
-setP_coord(coord::TrackCoord, value::Int) =
+setP_coord(coord::Coord, value::Int) =
     ccall((:coord_setP, LIB_ODRMGR), Void, (Ptr{Void}, Ptr{Integer}), coord.ptr, value)
 
-setR_coord(coord::TrackCoord, value::Int) =
+setR_coord(coord::Coord, value::Int) =
     ccall((:coord_setR, LIB_ODRMGR), Void, (Ptr{Void}, Ptr{Integer}), coord.ptr, value)
 
-intitialize_lanecoord(coord::TrackCoord) =
+intitialize_coord(coord::Coord) =
     ccall((:coord_init, LIB_ODRMGR), Void, (Ptr{Void},), coord.ptr)
   
-print_coord(coord::TrackCoord) =
+print_coord(coord::Coord) =
     ccall((:coord_print, LIB_ODRMGR), Void, (Ptr{Void}, ), coord.ptr)
+
+get_value_coord(coord::Coord) =
+    ccall((:coord_getValue, LIB_ODRMGR), Double, (Ptr{Void},), coord.ptr)
+    
