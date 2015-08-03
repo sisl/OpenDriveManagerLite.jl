6db5650f674c0e2cf9b24df18aec0d8044f17a15
depet6 <depet6@dn0a22c866.sunet>
2015-08-03 09:42:30 -0700

    misc

--- src/includes/trackcoord.jl
+++ src/includes/trackcoord.jl
@@ -34,10 +34,9 @@ type TrackCoord
     p::Cdouble    # pitch [rad]
     r::Cdouble    # roll [rad]
 
-    TrackCoord() =
-        trackcoord = new() # create one without setting any values
+    TrackCoord() = new() # create one without setting any values
     TrackCoord(trackid::Integer, s::Real, t::Real, z::Real=0.0, h::Real=0.0, p::Real=0.0, r::Real=0.0) =
-        trackcoord =  new(trackid, s, t, z, h, p, r)
+        new(trackid, s, t, z, h, p, r)
 
 end
 
