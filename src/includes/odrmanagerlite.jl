e98860dc96faf016f19d22ac1d67f665e2e8305f
depet6 <depet6@dn0a22c866.sunet>
2015-08-03 14:28:42 -0700

    misc

--- src/includes/odrmanagerlite.jl
+++ src/includes/odrmanagerlite.jl
@@ -31,16 +31,15 @@ type OdrManagerLite
     ptr::Ptr{Void}
     has_activated_position::Bool
     
-     
-     OdrManagerLite()=
-        # ptr = 
-        ccall( (:createOdrManagerLite, LIB_ODRMGR), Ptr{Void}, () )
-        # odrmanager = new(ptr, false)
-    #     finalizer(odrmanager, obj -> begin
-    #         ccall( (:free_OdrManagerLite, LIB_ODRMGR), Void, (Ptr{Void},), obj.ptr )
-    #     end)
-    #     odrmanager
-    # end
+    function OdrManagerLite()
+        # ptr = ccall( (:createOdrManagerLite, LIB_ODRMGR), (Ptr{Void},), () )
+        ptr = ccall( (:createOdrManagerLite, LIB_ODRMGR), Ptr{Void}, () )
+        odrmanager = new(ptr, false)
+        # finalizer(odrmanager, obj -> begin
+        #     ccall( (:free_OdrManagerLite, LIB_ODRMGR), Void, (Ptr{Void},), obj.ptr )
+        # end)
+        # odrmanager
+    end
 end
 
 type Position
