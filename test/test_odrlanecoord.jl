7a4a8896b46a4bbd01a987875d12a2272dff0ad1
depet6 <depet6@dn0a22c866.sunet>
2015-08-03 13:52:57 -0700

    misc

--- test/test_odrlanecoord.jl
+++ test/test_odrlanecoord.jl
@@ -1,13 +1,13 @@
-coord = LaneCoord(0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 1, 1.0)   # distance perpendicular to the track
-@test coord.trackid == 0
-@test coord.s == 1.0
-@test coord.t == 2.0
-@test coord.z == 3.0
-@test coord.h == 4.0
-@test coord.p == 5.0
-@test coord.r == 6.0
-@test coord.laneid == 1
-@test coord.offset == 1.0
+# coord = LaneCoord(0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 1, 1.0)   # distance perpendicular to the track
+# @test coord.trackid == 0
+# @test coord.s == 1.0
+# @test coord.t == 2.0
+# @test coord.z == 3.0
+# @test coord.h == 4.0
+# @test coord.p == 5.0
+# @test coord.r == 6.0
+# @test coord.laneid == 1
+# @test coord.offset == 1.0
 
 # coordA = copy(coord)
 # @test coordA.trackid == 0
