564f66fe586b035f7b549dcad97d7ee9c4176bb9
depet6 <depet6@dn51s4s5.sunet>
2015-08-02 20:27:07 -0700

    misc

--- src/includes/trackcoord.jl
+++ src/includes/trackcoord.jl
@@ -47,7 +47,7 @@ type TrackCoord
         end)
         trackcoord
     end
-     function TrackCoord(trackid::Int, s::Real, t::Real, z::Real=0.0, h::Real=0.0, p::Real=0.0, r::Real=0.0)
+     function TrackCoord(trackid::Cint, s::Cdouble, t::Cdouble, z::Cdouble=0.0, h::Cdouble=0.0, p::Cdouble=0.0, r::Cdouble=0.0)
         trackcoord =  new(trackid, s, t, z, h, p, r)
         finalizer(trackcoord, obj ->begin
             ccall((:freeTrackCoord, LIB_ODRMGR), Void, (Ptr{Void},), obj.prt)
