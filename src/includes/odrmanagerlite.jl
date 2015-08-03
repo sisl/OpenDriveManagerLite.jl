48d6d4b177a58db71d3422eb4f98bb78223e08b9
depet6 <depet6@dn51s4s5.sunet>
2015-08-02 22:30:53 -0700

    misc

--- src/includes/odrmanagerlite.jl
+++ src/includes/odrmanagerlite.jl
@@ -120,8 +120,8 @@ end
 set_pos(mgr::OdrManagerLite, value::OdrManagerLite) =
     ccall((:odr_manager_setpos_track_coord, LIB_ODRMGR), Void, (Ptr{Void}, Ptr{TrackCoord}), mgr.ptr, value.ptr)
 
-set_trackpos_s_t(mgr::OdrManagerLite, id::Cint, s::Cdouble, t::Cdouble=0.0) =
-    ccall((:odr_manager_set_track_pos_s_t, LIB_ODRMGR), Void, (Ptr{Void}, Ptr{Int}, Ptr{Cdouble}, Ptr{Cdouble}), mgr.ptr, id, s, t)
+set_trackpos_s_t(mgr::OdrManagerLite, id::Int, s::Real, t::Real=0.0) =
+    ccall((:odr_manager_set_track_pos_s_t, LIB_ODRMGR), Void, (Ptr{Void}, Ptr{Int}, Ptr{Double}, Ptr{Double}), mgr.ptr, id, s, t)
 
 set_trackpos_track_coord(mgr::OdrManagerLite, value::OdrManagerLite) =
     ccall((:odr_manager_set_track_pos_track_coord, LIB_ODRMGR), Void, (Ptr{Void},
