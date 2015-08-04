1d7fcf0de938c49b36b8353854b13814565fe0a1
depet6 <depet6@dn51s4s5.sunet>
2015-08-03 22:36:15 -0700

    creating test for julia functions

--- test/test_odrlanecoord.jl
+++ test/test_odrlanecoord.jl
@@ -35,8 +35,24 @@ coordB.s = 5.5
 
 track_coord_plusequal(coordA, coordB)
 
-# lane_coord_plusequal(copy(coordA), coordB)
+lane_coord_plusequal(copy(coordA), coordB)
 
-# intitialize_lanecoord(coord)
+intitialize_lanecoord(coord)
 
-# print_lanecoord(coord)
+print_lanecoord(coord)
+
+
+
+coordA = TrackCoord(0, 1.0 ,2.0 ,3.0 ,4.0 ,5.0 ,6.0)
+@test coord.trackid == 0
+@test coord.s == 1.0
+@test coord.t == 2.0
+@test coord.z == 3.0
+@test coord.h == 4.0
+@test coord.p == 5.0
+@test coord.r == 6.0
+ 
+ coordB = coordA
+ coordB.s = 4.0
+
+track_coord_plusequal(coordA, coordB)
