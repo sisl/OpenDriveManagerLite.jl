dab255201a52fa1cf7465f1b6b6562977edc07af
depet6 <depet6@dn51s4s5.sunet>
2015-08-03 22:30:59 -0700

    creating test for julia functions

--- test/test_odrlanecoord.jl
+++ test/test_odrlanecoord.jl
@@ -1,15 +1,15 @@
-# coord = LaneCoord(0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 1, 1.0)   # distance perpendicular to the track
-# @test coord.trackid == 0
-# @test coord.s == 1.0
-# @test coord.t == 2.0
-# @test coord.z == 3.0
-# @test coord.h == 4.0
-# @test coord.p == 5.0
-# @test coord.r == 6.0
-# @test coord.laneid == 1
-# @test coord.offset == 1.0
+coord = LaneCoord(0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 1, 1.0)   # distance perpendicular to the track
+@test coord.trackid == 0
+@test coord.s == 1.0
+@test coord.t == 2.0
+@test coord.z == 3.0
+@test coord.h == 4.0
+@test coord.p == 5.0
+@test coord.r == 6.0
+@test coord.laneid == 1
+@test coord.offset == 1.0
 
-# coordA = copy(coord)
+# coordA = coord
 # @test coordA.trackid == 0
 # @test coordA.s == 1.0
 # @test coordA.t == 2.0
@@ -20,7 +20,7 @@
 # @test coord.laneid == 1
 # @test coord.offset == 1.0
 
-# coordB = deepcopy(coord)
+# coordB = coord
 # @test coordB.trackid == 0
 # @test coordB.s == 1.0
 # @test coordB.t == 2.0
