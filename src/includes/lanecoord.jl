07b0ab9ab740e5f8a0b40b3efe9e919a0a8cb095
Tim Wheeler <wheelert@stanford.edu>
2015-08-09 11:41:04 -0700

    added immutables

--- src/includes/lanecoord.jl
+++ src/includes/lanecoord.jl
@@ -1,7 +1,10 @@
 export
-    LaneCoord
+    AbstractLaneCoord,
+    LaneCoord,
+    LaneCoordIm
 
-type LaneCoord <: AbstractTrackCoord
+abstract AbstractLaneCoord <: AbstractTrackCoord
+type LaneCoord <: AbstractLaneCoord
     trackid::Cint # id of the corresponding track
     s::Cdouble    # distance along the track
     t::Cdouble    # distance perpendicular to the track
@@ -16,25 +19,49 @@ type LaneCoord <: AbstractTrackCoord
     LaneCoord(trackid::Integer, s::Real, t::Real, z::Real = 0.0, h::Real=0.0, p::Real=0.0, r::Real=0.0, laneid::Real=1, offset::Real=0.0 ) =
         new(trackid, s, t, z, h, p, r, laneid, offset)
 end
+immutable LaneCoordIm <: AbstractLaneCoord
+    trackid::Cint # id of the corresponding track
+    s::Cdouble    # distance along the track
+    t::Cdouble    # distance perpendicular to the track
+    z::Cdouble    # height
+    h::Cdouble    # heading [rad]
+    p::Cdouble    # pitch [rad]
+    r::Cdouble 
+    laneid::Cint
+    offset::Cdouble
 
-function ==(a::LaneCoord, b::LaneCoord)
-    a.trackid == b.trackid &&
-    a.s == b.s &&
-    a.t == b.t &&
-    a.z == b.z &&
-    a.h == b.h &&
-    a.p == b.p &&
-    a.r == b.r &&
-    a.laneid == b.laneid &&
-    a.offset == b.offset
+    LaneCoordIm(trackid::Integer, s::Real, t::Real, z::Real = 0.0, h::Real=0.0, p::Real=0.0, r::Real=0.0, laneid::Real=1, offset::Real=0.0 ) =
+        new(trackid, s, t, z, h, p, r, laneid, offset)    
 end
 
-show(io::IO, coord::LaneCoord) = @printf(io, "(%d, %.16e, %.16e, %.16e, %.16e, %.16e, %.16e, %d, %.16e)", coord.trackid, coord.s, coord.t, coord.z, coord.h, coord.p, coord.r, coord.laneid, coord.offset)
-print(io::IO, coord::LaneCoord) = @printf(io, "(%d, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %d, %.3f)", coord.trackid, coord.s, coord.t, coord.z, coord.h, coord.p, coord.r, coord.laneid, coord.offset)
+function ==(a::AbstractLaneCoord, b::AbstractLaneCoord)
+    a.trackid == b.trackid &&
+    isapprox(a.s, b.s) &&
+    isapprox(a.t, b.t) &&
+    isapprox(a.z, b.z) &&
+    isapprox(a.h, b.h) &&
+    isapprox(a.p, b.p) &&
+    isapprox(a.r, b.r) &&
+    a.laneid == b.laneid &&
+    isapprox(a.offset, b.offset)
+end
+
+function show(io::IO, coord::AbstractLaneCoord)
+    @printf(io, "(%d, %.16e, %.16e, %.16e, %.16e, %.16e, %.16e, %d, %.16e)", 
+                  coord.trackid, coord.s, coord.t, coord.z, 
+                  coord.h, coord.p, coord.r, coord.laneid, coord.offset)
+end
+function print(io::IO, coord::AbstractLaneCoord)
+    @printf(io, "(%d, %.3f, %.3f, %.3f, %.3f, %.3f, %.3f, %d, %.3f)",
+                  coord.trackid, coord.s, coord.t, coord.z, 
+                  coord.h, coord.p, coord.r, coord.laneid, coord.offset)
+end
 
 copy(c::LaneCoord) = LaneCoord(c.trackid, c.s, c.t, c.z, c.h, c.p, c.r, c.laneid, c.offset)
-deepcopy(coord::LaneCoord) = copy(coord)
-function copy!(dest::LaneCoord, src::LaneCoord)
+copy(c::LaneCoordIm) = LaneCoordIm(c.trackid, c.s, c.t, c.z, c.h, c.p, c.r, c.laneid, c.offset)
+deepcopy(coord::AbstractLaneCoord) = copy(coord)
+
+function copy!(dest::LaneCoord, src::AbstractLaneCoord)
     dest.trackid = src.trackid
     dest.s = src.s
     dest.t = src.t
@@ -47,7 +74,7 @@ function copy!(dest::LaneCoord, src::LaneCoord)
     dest
 end
 
-function plus!(a::LaneCoord, b::LaneCoord)
+function plus!(a::LaneCoord, b::AbstractLaneCoord)
     (ccall( (:lanecoord_plusequal_lanecoord, LIB_ODRMGR), Void, (Ptr{Void},Ptr{Void}), 
         pointer_from_objref(a), pointer_from_objref(b)))
     a
--- test/test_lanecoord.jl
+++ test/test_lanecoord.jl
@@ -1,15 +1,41 @@
-coord = LaneCoord(0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 1, 1.0)   # distance perpendicular to the track
+coord = LaneCoord(0, 1.0, 2.0, 3.0)   # distance perpendicular to the track
 @test coord.trackid == 0
 @test coord.s == 1.0
 @test coord.t == 2.0
 @test coord.z == 3.0
-@test coord.h == 4.0
-@test coord.p == 5.0
-@test coord.r == 6.0
+@test coord.h == 0.0
+@test coord.p == 0.0
+@test coord.r == 0.0
 @test coord.laneid == 1
-@test coord.offset == 1.0
+@test coord.offset == 0.0
 
+@test coord == LaneCoord(0, 1.0, 2.0, 3.0, 0.0, 0.0, 0.0, 1, 0.0)
+
+coord = LaneCoord(0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 1, 1.0)   # distance perpendicular to the track
 @test coord == LaneCoord(0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 1, 1.0)
+
+coordim = LaneCoordIm(0, 1.0, 2.0, 3.0)
+@test coordim.trackid == 0
+@test coordim.s == 1.0
+@test coordim.t == 2.0
+@test coordim.z == 3.0
+@test coordim.h == 0.0
+@test coordim.p == 0.0
+@test coordim.r == 0.0
+@test coordim.laneid == 1
+@test coordim.offset == 0.0
+
+@test coordim == LaneCoord(0, 1.0, 2.0, 3.0, 0.0, 0.0, 0.0, 1, 0.0)
+@test coordim == LaneCoordIm(0, 1.0, 2.0, 3.0, 0.0, 0.0, 0.0, 1, 0.0)
+
+coordim = LaneCoordIm(0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 1, 1.0)
+@test coord == coordim
+@test coordim == coord
+
+show(coord); print("\n")
+show(coordim); print("\n")
+print(coord, "\n")
+print(coordim, "\n")
 
 @test copy(coord) == LaneCoord(0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 1, 1.0)
 @test deepcopy(coord) == LaneCoord(0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 1, 1.0)
@@ -18,8 +44,10 @@ coord = LaneCoord(0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 1, 1.0)   # distance perpendi
 
 @test plus!(copy(coord), LaneCoord(1, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 2, 6.5)) == 
         LaneCoord(1, 1.5, 3.5, 5.5, 7.5, 5.0, 6.0, 3, 7.5)
-
 @test plus!(copy(coord), TrackCoord(1, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5)) == 
         LaneCoord(1, 1.5, 3.5, 5.5, 7.5, 5.0, 6.0, 1, 1.0)
+
+@test copy(coordim) == LaneCoordIm(0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 1, 1.0)
+@test deepcopy(coord) == LaneCoord(0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 1, 1.0)
 
 
